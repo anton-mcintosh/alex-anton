e1572c63cbb518e950af80dde9a7400b75332713
Alex Crocker <imdacrocker@gmail.com>
2025-03-06 13:56:00 -0800

    Updated to use the new NimBLE task

--- GoProCanBusController/main/goPro_canBus_main.c
+++ GoProCanBusController/main/goPro_canBus_main.c
@@ -120,12 +120,9 @@ void init_spiffs(void)
 }
 void app_main(void)
 {
-    /* Local variables */
-    int rc = 0;
-    esp_err_t ret = ESP_OK;
 
     /* NVS flash initialization */
-    ret = nvs_flash_init();
+    esp_err_t ret = nvs_flash_init();
     if (ret == ESP_ERR_NVS_NO_FREE_PAGES ||
         ret == ESP_ERR_NVS_NEW_VERSION_FOUND)
     {
@@ -143,28 +140,7 @@ void app_main(void)
     init_spiffs();
     wifi_init_softap();
     server_initiation();
+    ble_client_init();
 
-    /* NimBLE stack initialization */
-    ret = nimble_port_init();
-    if (ret != ESP_OK)
-    {
-        ESP_LOGE(TAG, "failed to initialize nimble stack, error code: %d ",
-                 ret);
-        return;
-    }
-
-    /* GAP service initialization */
-    rc = gap_init();
-    if (rc != 0)
-    {
-        ESP_LOGE(TAG, "failed to initialize GAP service, error code: %d", rc);
-        return;
-    }
-
-    /* NimBLE host configuration initialization */
-    nimble_host_config_init();
-
-    /* Start NimBLE host task thread and return */
-    xTaskCreate(nimble_host_task, "NimBLE Host", 4 * 1024, NULL, 5, NULL);
     return;
 }
