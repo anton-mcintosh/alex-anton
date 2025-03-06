d0de8db30a41a1a1bf94a77f87418f28e2631c76
Alex Crocker <imdacrocker@gmail.com>
2025-03-06 11:37:49 -0800

    Switched to a different method to initiate the NIMble

--- GoProCanBusController/main/goPro_canBus_main.c
+++ GoProCanBusController/main/goPro_canBus_main.c
@@ -14,10 +14,8 @@
 #include "nvs_flash.h"
 #include "esp_types.h"
 #include "esp_http_server.h"
-#include <netdb.h>
-#include "nvs_flash.h"
 #include "esp_spiffs.h"
-
+#include <netdb.h>
 
 #include "lwip/netdb.h"
 #include "lwip/etharp.h"
@@ -28,47 +26,145 @@
 #include "softAP.h"
 #include "webServer.h"
 #include "ble_Client.h"
+#include "gap.h"
 
+/* NimBLE stack APIs */
+#include "host/ble_hs.h"
+#include "host/ble_uuid.h"
+#include "host/util/util.h"
+#include "nimble/ble.h"
+#include "nimble/nimble_port.h"
+#include "nimble/nimble_port_freertos.h"
+
+/* NimBLE GAP APIs */
+#include "host/ble_gap.h"
+#include "services/gap/ble_svc_gap.h"
 
 static const char *TAG = "wifi softAP";
 
-void init_spiffs(void) {
+/* Library function declarations */
+void ble_store_config_init(void);
+
+/* Private function declarations */
+static void on_stack_reset(int reason);
+static void on_stack_sync(void);
+static void nimble_host_config_init(void);
+static void nimble_host_task(void *param);
+
+/* Private functions */
+/*
+ *  Stack event callback functions
+ *      - on_stack_reset is called when host resets BLE stack due to errors
+ *      - on_stack_sync is called when host has synced with controller
+ */
+static void on_stack_reset(int reason)
+{
+    /* On reset, print reset reason to console */
+    ESP_LOGI(TAG, "nimble stack reset, reset reason: %d", reason);
+}
+
+static void on_stack_sync(void)
+{
+    /* On stack sync, do advertising initialization */
+    adv_init();
+}
+
+static void nimble_host_config_init(void)
+{
+    /* Set host callbacks */
+    ble_hs_cfg.reset_cb = on_stack_reset;
+    ble_hs_cfg.sync_cb = on_stack_sync;
+    ble_hs_cfg.store_status_cb = ble_store_util_status_rr;
+
+    /* Store host configuration */
+    ble_store_config_init();
+}
+
+static void nimble_host_task(void *param)
+{
+    /* Task entry log */
+    ESP_LOGI(TAG, "nimble host task has been started!");
+
+    /* This function won't return until nimble_port_stop() is executed */
+    nimble_port_run();
+
+    /* Clean up at exit */
+    vTaskDelete(NULL);
+}
+
+void init_spiffs(void)
+{
     esp_vfs_spiffs_conf_t conf = {
         .base_path = "/spiffs",
-        .partition_label = "storage",  // Use the label from partitions.csv
+        .partition_label = "storage", // Use the label from partitions.csv
         .max_files = 5,
-        .format_if_mount_failed = true
-    };
+        .format_if_mount_failed = true};
 
     esp_err_t ret = esp_vfs_spiffs_register(&conf);
-    if (ret != ESP_OK) {
+    if (ret != ESP_OK)
+    {
         ESP_LOGE(TAG, "Failed to mount SPIFFS: %s", esp_err_to_name(ret));
         return;
     }
 
     size_t total = 0, used = 0;
     ret = esp_spiffs_info("storage", &total, &used);
-    if (ret != ESP_OK) {
+    if (ret != ESP_OK)
+    {
         ESP_LOGE(TAG, "Failed to get SPIFFS partition information: %s", esp_err_to_name(ret));
-    } else {
+    }
+    else
+    {
         ESP_LOGI(TAG, "Partition size: total: %d, used: %d", total, used);
     }
 }
-
 void app_main(void)
 {
-    //Initialize NVS
-    esp_err_t ret = nvs_flash_init();
-    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
-      ESP_ERROR_CHECK(nvs_flash_erase());
-      ret = nvs_flash_init();
+    /* Local variables */
+    int rc = 0;
+    esp_err_t ret = ESP_OK;
+
+    /* NVS flash initialization */
+    ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES ||
+        ret == ESP_ERR_NVS_NEW_VERSION_FOUND)
+    {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
     }
-    ESP_ERROR_CHECK(ret);
-    
+    if (ret != ESP_OK)
+    {
+        ESP_LOGE(TAG, "failed to initialize nvs flash, error code: %d ", ret);
+        return;
+    }
+
     ESP_LOGI(TAG, "ESP_WIFI_MODE_AP");
 
     init_spiffs();
     wifi_init_softap();
     server_initiation();
-    ble_client_init();
+
+    /* NimBLE stack initialization */
+    ret = nimble_port_init();
+    if (ret != ESP_OK)
+    {
+        ESP_LOGE(TAG, "failed to initialize nimble stack, error code: %d ",
+                 ret);
+        return;
+    }
+
+    /* GAP service initialization */
+    rc = gap_init();
+    if (rc != 0)
+    {
+        ESP_LOGE(TAG, "failed to initialize GAP service, error code: %d", rc);
+        return;
+    }
+
+    /* NimBLE host configuration initialization */
+    nimble_host_config_init();
+
+    /* Start NimBLE host task thread and return */
+    xTaskCreate(nimble_host_task, "NimBLE Host", 4 * 1024, NULL, 5, NULL);
+    return;
 }
